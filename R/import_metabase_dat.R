23273dffdf9282e45081a47b1f249dbd8058f232
Madsen <Chris.Madsen@gov.bc.ca>
2025-04-07 15:35:50 -0700

    Added new section to treat new metabase data with accurate timezones differently from old metabase data that needed manual corrections

--- R/import_metabase_dat.R
+++ R/import_metabase_dat.R
@@ -19,7 +19,30 @@ import_metabase_dat = function(my_opts){
   rm(metabase_data_list)
 
   # Use a pretty complicated bifurcating function to clean up the shift start and end time fields.
-  metabase_dat = correct_shift_start_and_end_times(metabase_dat)
+  # The developers corrected timezone application to inspections and the new app that does
+  # this was implemented on 2025-04-07. Any record that has a datetime in the Inspection Time
+  # column is part of this new tranche of data. This tranche sets all times to UTC. To find
+  # Pacific time, use lubridate's function "with_tz", it is awesome!
+  metabase_dat_utc_timezone = metabase_dat |>
+    dplyr::filter(stringr::str_detect(`Inspection Time`,"^[0-9\\-]{10}")) |>
+    dplyr::mutate(start_time_dt = lubridate::ymd_hms(`Start Time`),
+                  end_time_dt = lubridate::ymd_hms(`End Time`)) |>
+    dplyr::mutate(start_time_pst = lubridate::with_tz(start_time_dt, tzone = "America/Los_Angeles"),
+                  end_time_pst = lubridate::with_tz(end_time_dt, tzone = "America/Los_Angeles")) |>
+    dplyr::mutate(start_time_pst = as.character(start_time_pst),
+                  end_time_pst = as.character(end_time_pst)) |>
+    dplyr::select(-c(`Start Time`,`End Time`,start_time_dt,end_time_dt)) |>
+    dplyr::rename(`Start Time` = start_time_pst,
+                  `End Time` = end_time_pst,
+                  Workflow_ID = `Workflow ID`)
+
+  metabase_dat_old_timezones = metabase_dat |>
+    dplyr::filter(!stringr::str_detect(`Inspection Time`,"^[0-9\\-]{10}") | is.na(`Inspection Time`))
+
+  metabase_dat_old_timezones = correct_shift_start_and_end_times(metabase_dat_old_timezones)
+
+  # Recombing metabase data, now with datetime fields more or less correct!
+  metabase_dat = dplyr::bind_rows(metabase_dat_old_timezones, metabase_dat_utc_timezone)
 
   return(metabase_dat)
 }
